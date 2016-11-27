3e328b8ec662c93533879cb4a49c2b3673f8c57d
LakshayS <lakshay13@gmail.com>
2016-11-27 19:01:50 +0100

    Commit Main class with Java doc style comments.

--- SpockProject/src/main/java/spockbasics/MathOperations.java
+++ SpockProject/src/main/java/spockbasics/MathOperations.java
@@ -11,16 +11,35 @@ public class MathOperations {
 
     Logger logger = Logger.getLogger("Math Operations");
 
+    /**
+     * Method that gets the modulus of two values
+     * @param a input value 1
+     * @param b input value 2
+     * @return the modulus value
+     */
     public int getModulusResult(int a, int b) {
         logger.info("Math modulus operation");
         return a%b;
     }
 
+    /**
+     * Method that gets the subtraction of two values
+     * @param a input value 1
+     * @param b input value 2
+     * @return the subtracted result
+     */
     public int getSubtractionResult(int a, int b) {
         logger.info("Math subtraction operation");
         return a-b;
     }
 
+    /**
+     * Method that gets the division of two values
+     * @param a numerator of the division
+     * @param b denominator of the division
+     * @return the divided result
+     * @throws NumberFormatException if the denominator is 0
+     */
     public long getDivideResult(int a, int b) throws NumberFormatException {
 
         logger.info("Checking if denominator is 0");
